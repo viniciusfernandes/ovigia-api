1acf8a818845f1fdd1e6db8f3a864332ca1562ab
viniciusfernandes <viniciussf@hotmail.com>
2021-06-08 19:43:01 -0300

    Refatoracao da classe de mensagem de resposta de erro

--- src/main/java/br/com/ovigia/businessrule/Response.java
+++ src/main/java/br/com/ovigia/businessrule/Response.java
@@ -8,49 +8,20 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class Response<V> {
 	private final ResponseStatus status;
 	private final V value;
-	private List<String> mensagens;
 
 	private Response() {
 		status = ResponseStatus.NO_RESULT;
 		value = null;
-		mensagens = null;
 	}
 
-	private Response(ResponseStatus status, V value, String mensagem) {
+	Response(ResponseStatus status, V value) {
 		this.status = status;
 		this.value = value;
-		addMensagem(mensagem);
-	}
-
-	private Response(ResponseStatus status, V value, List<String> mensagens) {
-		this.status = status;
-		this.value = value;
-		this.mensagens = mensagens;
-	}
-
-	private Response(ResponseStatus status, V value) {
-		this.status = status;
-		this.value = value;
-		this.mensagens = null;
-	}
-
-	private Response(ResponseStatus status, List<String> mensagens) {
-		this.status = status;
-		this.value = null;
-		this.mensagens = mensagens;
 	}
 
 	private Response(V value) {
 		this.status = ResponseStatus.OK;
 		this.value = value;
-		this.mensagens = null;
-	}
-
-	public void addMensagem(String mensagem) {
-		if (mensagens == null) {
-			mensagens = new ArrayList<>();
-		}
-		mensagens.add(mensagem);
 	}
 
 	@JsonIgnore
@@ -78,14 +49,6 @@ public class Response<V> {
 		return ResponseStatus.UNPROCESSABLE_ENTITY == status;
 	}
 
-	public List<String> getMensagens() {
-		return mensagens;
-	}
-
-	public void setMensagens(List<String> mensagens) {
-		this.mensagens = mensagens;
-	}
-
 	public ResponseStatus getStatus() {
 		return status;
 	}
@@ -95,7 +58,7 @@ public class Response<V> {
 	}
 
 	public <T> Response<T> clone(T t) {
-		return new Response<>(status, t, mensagens);
+		return new Response<>(status, t);
 	}
 
 	public static <T> Response<T> ok(T value) {
@@ -107,12 +70,23 @@ public class Response<V> {
 	}
 
 	public static <T> Response<T> error(T value, String mensagem) {
-		return new Response<>(ResponseStatus.INTERNAL_SERVER_ERROR, value, mensagem);
+		return new ResponseMessage<>(ResponseStatus.INTERNAL_SERVER_ERROR, value, mensagem);
 	}
 
 	public static <T> Response<T> unprocessable(T value, String mensagem) {
-		return new Response<>(ResponseStatus.UNPROCESSABLE_ENTITY, value, mensagem);
+		return new ResponseMessage<>(ResponseStatus.UNPROCESSABLE_ENTITY, value, mensagem);
 	}
+
+	private static class ResponseMessage<V> extends Response<V> {
+		private String[] mensagens;
+
+		private ResponseMessage(ResponseStatus status, V value, String... mensagens) {
+			super(status, value);
+			this.mensagens = mensagens;
+		}
+
+	}
+
 }
 
 enum ResponseStatus {
