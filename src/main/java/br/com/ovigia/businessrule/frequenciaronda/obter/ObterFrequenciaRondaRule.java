0b4491d2f3df49881bf3fb0ee5861bc3a53a65a4
Vinicius <viniciussf@hotmail.com>
2022-06-28 18:49:41 -0300

    Formatacao de classe para teste

--- src/main/java/br/com/ovigia/businessrule/frequenciaronda/obter/ObterFrequenciaRondaRule.java
+++ src/main/java/br/com/ovigia/businessrule/frequenciaronda/obter/ObterFrequenciaRondaRule.java
@@ -9,56 +9,57 @@ import br.com.ovigia.model.repository.ClienteRepository;
 import br.com.ovigia.model.repository.VigiaRepository;
 import reactor.core.publisher.Mono;
 
-public class ObterFrequenciaRondaRule
-		implements BusinessRule<ObterFrequenciaRondaRequest, ObterFrequenciaRondaResponse> {
-	private ClienteRepository clienteRepository;
-	private CriarFrequenciaRondasBusiness criarFrequenciaRondasBusiness;
-	private VigiaRepository vigiaRepository;
+public class ObterFrequenciaRondaRule implements BusinessRule<ObterFrequenciaRondaRequest, ObterFrequenciaRondaResponse> {
+  private final ClienteRepository clienteRepository;
+  private final CriarFrequenciaRondasBusiness criarFrequenciaRondasBusiness;
+  private final VigiaRepository vigiaRepository;
 
-	public ObterFrequenciaRondaRule(ClienteRepository clienteRepository, VigiaRepository vigiaRepository,
-			CriarFrequenciaRondasBusiness criarFrequenciaRondasBusiness) {
-		this.clienteRepository = clienteRepository;
-		this.vigiaRepository = vigiaRepository;
-		this.criarFrequenciaRondasBusiness = criarFrequenciaRondasBusiness;
-	}
+  public ObterFrequenciaRondaRule(ClienteRepository clienteRepository, VigiaRepository vigiaRepository, CriarFrequenciaRondasBusiness criarFrequenciaRondasBusiness) {
 
-	@Override
-	public Mono<Response<ObterFrequenciaRondaResponse>> apply(ObterFrequenciaRondaRequest request) {
-		return obterFrequenciaRonda(request).map(frequencia -> {
-			var response = new ObterFrequenciaRondaResponse();
-			response.dataUltimaRonda = DataUtil.formatarData(frequencia.dataUltimaRonda);
-			response.totalRonda = frequencia.totalRonda;
-			return Response.ok(response);
-		}).switchIfEmpty(Mono.just(Response.ok(new ObterFrequenciaRondaResponse())));
-	}
+    this.clienteRepository = clienteRepository;
+    this.vigiaRepository = vigiaRepository;
+    this.criarFrequenciaRondasBusiness = criarFrequenciaRondasBusiness;
+  }
 
-	private Mono<FrequenciaRonda> obterFrequenciaRonda(ObterFrequenciaRondaRequest request) {
-		return clienteRepository.obterFrequenciaRondaPorIdCliente(request.idCliente).flatMap(frequencia -> {
-			if (frequencia.isExistente()) {
-				return recalcularFrequenciaRonda(frequencia, request.idCliente);
-			}
-			return calcularFrequenciaRonda(request.idCliente);
-		});
+  @Override
+  public Mono<Response<ObterFrequenciaRondaResponse>> apply(ObterFrequenciaRondaRequest request) {
 
-	}
+    return obterFrequenciaRonda(request).map(frequencia -> {
+      var response = new ObterFrequenciaRondaResponse();
+      response.dataUltimaRonda = DataUtil.formatarData(frequencia.dataUltimaRonda);
+      response.totalRonda = frequencia.totalRonda;
+      return Response.ok(response);
+    }).switchIfEmpty(Mono.just(Response.ok(new ObterFrequenciaRondaResponse())));
+  }
 
-	private Mono<FrequenciaRonda> recalcularFrequenciaRonda(FrequenciaRonda frequencia, String idCliente) {
-		return vigiaRepository.obterDataUltimaRonda(frequencia.idVigia).flatMap(vigia -> {
-			// Essa condicao existe pois o vigia pode incluir as rondas parcialmente, assim
-			// temos que garantir que, se a ronda
-			// foi atualizada, devemos recalcular a frequencia de ronda para o cliente
-			if (frequencia.isDesatualizada(vigia.dataAtualizacaoRonda)) {
-				return criarFrequenciaRondasBusiness.apply(idCliente, vigia.dataAtualizacaoRonda);
-			}
-			return Mono.just(frequencia);
-		});
-	}
+  private Mono<FrequenciaRonda> obterFrequenciaRonda(ObterFrequenciaRondaRequest request) {
 
-	private Mono<FrequenciaRonda> calcularFrequenciaRonda(String idCliente) {
-		return clienteRepository.obterIdVigiaELocalizacaoByIdCliente(idCliente)
-				.flatMap(cliente -> vigiaRepository.obterDataUltimaRonda(cliente.idVigia))
-				.flatMap(vigia -> criarFrequenciaRondasBusiness.apply(idCliente, vigia.dataAtualizacaoRonda));
+    return clienteRepository.obterFrequenciaRondaPorIdCliente(request.idCliente).flatMap(frequencia -> {
+      if (frequencia.isExistente()) {
+        return recalcularFrequenciaRonda(frequencia, request.idCliente);
+      }
+      return calcularFrequenciaRonda(request.idCliente);
+    });
 
-	}
+  }
+
+  private Mono<FrequenciaRonda> recalcularFrequenciaRonda(FrequenciaRonda frequencia, String idCliente) {
+
+    return vigiaRepository.obterDataUltimaRonda(frequencia.idVigia).flatMap(vigia -> {
+      // Essa condicao existe pois o vigia pode incluir as rondas parcialmente, assim
+      // temos que garantir que, se a ronda
+      // foi atualizada, devemos recalcular a frequencia de ronda para o cliente
+      if (frequencia.isDesatualizada(vigia.dataAtualizacaoRonda)) {
+        return criarFrequenciaRondasBusiness.apply(idCliente, vigia.dataAtualizacaoRonda);
+      }
+      return Mono.just(frequencia);
+    });
+  }
+
+  private Mono<FrequenciaRonda> calcularFrequenciaRonda(String idCliente) {
+
+    return clienteRepository.obterIdVigiaELocalizacaoByIdCliente(idCliente).flatMap(cliente -> vigiaRepository.obterDataUltimaRonda(cliente.idVigia)).flatMap(vigia -> criarFrequenciaRondasBusiness.apply(idCliente, vigia.dataAtualizacaoRonda));
+
+  }
 
 }
